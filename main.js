9352f83ae4d92f0e7a73d21579155f33f95d0bf1
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2023-11-13 20:04:05 -0300

    Att project

---
+++ main.js
@@ -0,0 +1,17 @@
+var iconBar = document.querySelector('.faq-icon img');
+var textBar = document.querySelector('.text-faq');
+
+iconBar.addEventListener('click',function() {
+    
+    if (textBar.style.display == 'block') {
+        textBar.style.display = 'none';
+    } else {
+        textBar.style.display = 'none';
+    }
+});
+
+
+
+
+
+
